46e79655b5fc4f9030583a5df33fbaf47c561e9d
kiwenlau <kiwenlau@126.com>
2017-08-25 14:58:46 +0800

    add fundebug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+/*global document*/
+
 import _ from 'lodash';
+import fundebug from 'fundebug-javascript';
+fundebug.apikey = "3cf3b58896cde530559232dac62ede3898cd015f6d8ac0f3eb8f792346c27983";
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+function component()
+{
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 }
 
 document.body.appendChild(component());
